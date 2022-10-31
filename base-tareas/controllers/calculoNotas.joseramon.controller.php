fbeb11d92e4c390a7c8fe84fda849543ab7fc03c
josechor <joserrapaz5@gmail.com>
2022-10-31 11:03:19 +0100

    TextArea creado

---
+++ base-tareas/controllers/calculoNotas.joseramon.controller.php
@@ -0,0 +1,8 @@
+<?php
+$data = [];
+$data['titulo'] = "Plantilla formulario";
+$data["div_titulo"] = "Formulario";
+
+include 'views/templates/header.php';
+include 'views/calculoNotas.joseramon.view.php';
+include 'views/templates/footer.php';
---
+++ base-tareas/views/calculoNotas.joseramon.view.php
@@ -0,0 +1,34 @@
+<!-- Page Heading -->
+<div class="d-sm-flex align-items-center justify-content-between mb-4">
+    <h1 class="h3 mb-0 text-gray-800"><?php echo $data['titulo']; ?></h1>
+
+</div>
+
+<!-- Content Row -->
+
+<div class="row">
+
+    <div class="col-12">
+        <div class="card shadow mb-4">
+            <div
+                class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
+                <h6 class="m-0 font-weight-bold text-primary"><?php echo $data['div_titulo']; ?></h6>                                    
+            </div>
+            <!-- Card Body -->
+            <div class="card-body">
+                <!--<form action="./?sec=formulario" method="post">                   -->
+                <form method="post" action="./?sec=calculoNotas.joseramon">
+                    <input type="hidden" name="sec" value="formulario" />
+                    <div class="mb-3">
+                        <label for="exampleFormControlTextarea1">Exemplo textarea</label>
+                        <textarea class="form-control" id="exampleFormControlTextarea1" name="textarea" rows="3"></textarea>
+                    </div>
+                    <div class="mb-3">
+                        <input type="submit" value="Enviar" class="btn btn-primary"/>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>                        
+</div>
+
