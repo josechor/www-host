a0f6d39d186c62043640f9944cd1340cbbd45e3d
josechor <joserrapaz5@gmail.com>
2022-10-31 11:36:58 +0100

    checkForm

--- base-tareas/controllers/calculoNotas.joseramon.controller.php
+++ base-tareas/controllers/calculoNotas.joseramon.controller.php
@@ -1,7 +1,44 @@
 <?php
-$data = [];
-$data['titulo'] = "Plantilla formulario";
-$data["div_titulo"] = "Formulario";
+$data['resultado'] = [];
+
+if (isset($_POST['enviar'])) {
+    $data['errores'] = checkForm($_POST);
+}
+
+
+function checkForm($datos) : array
+{
+    $errores = [];
+    if (empty($datos['datos'])) {
+        $errores['datos'] = "Este campo es obligatorio";
+    } else {
+        $modulos = json_decode($datos['datos'], true);
+        if (json_last_error() !== JSON_ERROR_NONE) {
+            $errores['datos'] = "El formato no es correcto";
+        } else {
+            $erroresJson = "";
+            foreach ($modulos as $modulo => $alumnos) {
+                if (empty($modulo)) {
+                    $erroresJson .= "El nombre del módulo no puede estar vacio<br/>";
+                }
+                if (!is_array($alumnos)) {
+                    $erroresJson .= "El módulo " . htmlentities($modulo) . " no tiene un array de alumnos<br/>";
+                } else {
+                    foreach ($alumnos as $nombre => $nota) {
+                        if (empty($nombre)) {
+                            $erroresJson .= "El módulo " . htmlentities($modulo) . " tiene un alumno sin nombre<br/>";
+                        }
+                        if ($nota > 10 && $nota < 0) {
+                            $erroresJson .= "Módulo ".htmlentities($modulo). ", almuno ".htmlentities($nombre). "tiene una nota de $nota<br/>";
+                        }
+                    }
+                }
+            }
+            !empty($erroresJson) ? $errores['datos'] = $erroresJson : '';
+        }
+    }
+    return $errores;
+}
 
 include 'views/templates/header.php';
 include 'views/calculoNotas.joseramon.view.php';
--- base-tareas/views/calculoNotas.joseramon.view.php
+++ base-tareas/views/calculoNotas.joseramon.view.php
@@ -1,6 +1,6 @@
 <!-- Page Heading -->
 <div class="d-sm-flex align-items-center justify-content-between mb-4">
-    <h1 class="h3 mb-0 text-gray-800"><?php echo $data['titulo']; ?></h1>
+    <h1 class="h3 mb-0 text-gray-800">Trabajo notas</h1>
 
 </div>
 
@@ -12,19 +12,22 @@
         <div class="card shadow mb-4">
             <div
                 class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
-                <h6 class="m-0 font-weight-bold text-primary"><?php echo $data['div_titulo']; ?></h6>                                    
+                <h6 class="m-0 font-weight-bold text-primary">Trabajo notas</h6>                                    
             </div>
             <!-- Card Body -->
             <div class="card-body">
                 <!--<form action="./?sec=formulario" method="post">                   -->
                 <form method="post" action="./?sec=calculoNotas.joseramon">
+
                     <input type="hidden" name="sec" value="formulario" />
                     <div class="mb-3">
                         <label for="exampleFormControlTextarea1">Exemplo textarea</label>
-                        <textarea class="form-control" id="exampleFormControlTextarea1" name="textarea" rows="3"></textarea>
+                        <textarea class="form-control" id="datos" name="datos" rows="10"></textarea>
+                        <p class="text-danger small"><?php echo isset($data['errores']['datos']) ? $data['errores']['datos'] : ''; ?></p>
                     </div>
+
                     <div class="mb-3">
-                        <input type="submit" value="Enviar" class="btn btn-primary"/>
+                        <input type="submit" value="Enviar" name="enviar" class="btn btn-primary"/>
                     </div>
                 </form>
             </div>
