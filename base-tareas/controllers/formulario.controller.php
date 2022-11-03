8fae71f3b35fff9f6698cf908f80bc61367bb316
josechor <joserrapaz5@gmail.com>
2022-11-03 17:10:34 +0100

    eliminar innecesario

--- base-tareas/controllers/formulario.controller.php
+++
@@ -1,7 +0,0 @@
-<?php
-$data['titulo'] = "Plantilla formulario";
-$data["div_titulo"] = "Formulario";
-
-include 'views/templates/header.php';
-include 'views/formulario.view.php';
-include 'views/templates/footer.php';
